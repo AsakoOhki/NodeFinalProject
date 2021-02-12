52a8366910a54cd0af076da3d82931ca0ca0cd3a
Asako Ohki <ohkiasako@gmail.com>
2021-02-12 10:22:23 -0800

    First commit

---
+++ src/components/google-drive/Dashboard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Container } from "react-bootstrap"
+import { useFolder } from "../../hooks/useFolder"
+import AddFolderButton from './AddFolderButton'
+import Folder from "./Folder"
+import NavbarComponent from './Navbar'
+import FolderBreadcrumbs from "./FolderBreadcrumbs"
+import { useParams, useLocation } from "react-router-dom"
+
+export default function Dashboard() {
+  const { folderId } = useParams()
+  const { state = {} } = useLocation()
+  const { folder, childFolders } = useFolder(folderId,state.folder)
+
+  return (
+    <>
+      <NavbarComponent />
+      <Container fluid>
+        <div className="d-flex align-items-center">
+          <FolderBreadcrumbs currentFolder={folder} />
+          <AddFolderButton currentFolder={folder} />
+        </div>
+        {childFolders.length > 0 && (
+          <div className="d-flex flex-wrap">
+            {childFolders.map(childFolder => (
+              <div
+                key={childFolder.id}
+                style={{ maxWidth: "250px" }}
+                className="p-2"
+              >
+                <Folder folder={childFolder} />
+              </div>
+            ))}
+          </div>
+        )}
+      </Container>
+    </>
+  )
+}
